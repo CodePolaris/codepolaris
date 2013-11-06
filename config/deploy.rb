2cc34e094dd4178738937337ec179641b479f20e
Marcos Serpa <marcosserpa@gmail.com>
2013-11-05 23:36:09 -0200

    Deploy settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,47 +1,42 @@
 require "bundler/capistrano"
 require "rvm/capistrano"
-
-server "162.243.66.30", :web, :app, :db, primary: true
-
+ 
+server "162.243.70.102", :web, :app, :db, primary: true
+ 
 set :application, "codepolaris"
 set :user, "polaris"
-set :port, 2000
-set :deploy_to, "/home/#{user}/apps/#{application}"
-set :deploy_via, :remote_cache
+set :port, 22
+set :deploy_to, "/var/www/#{application}"
+set :deploy_via, :copy
 set :use_sudo, false
-
+set :rvm_ruby_string, "ruby-2.0.0-p247"
+ 
 set :scm, "git"
-set :repository, "git@github.com:marcosserpa/#{application}.git"
+set :repository, "git@github.com:marcosserpa/codepolaris.git"
 set :branch, "master"
-
-
+set :rvm_type, :system
+ 
+ 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-
+ 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
-
+ 
 namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
-    end
-  end
-
+ 
   task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
-
+ 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
+ 
   after "deploy:finalize_update", "deploy:symlink_config"
-
+ 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
@@ -50,5 +45,25 @@ namespace :deploy do
       exit
     end
   end
+ 
+  task :precompile_assets do
+    run <<-CMD
+      rm -rf #{latest_release}/public/assets &&
+      mkdir -p #{shared_path}/assets &&
+      ln -s #{shared_path}/assets #{latest_release}/public/assets
+    CMD
+    set(:answer, Capistrano::CLI.ui.ask("Do you want to skip asset compilation? (only 'yes' will skip)") )
+    if answer != 'yes'  
+      run_locally "rake assets:precompile"
+      run_locally "cd public; tar -zcvf assets.tar.gz assets"
+      top.upload "public/assets.tar.gz", "#{shared_path}", :via => :scp
+      run "cd #{shared_path}; tar -zxvf assets.tar.gz; rm assets.tar.gz"
+      run_locally "rm public/assets.tar.gz"
+      run_locally "rm -rf public/assets"
+    end
+  end
+ 
+  after 'deploy:update_code', 'deploy:precompile_assets'
+ 
   before "deploy", "deploy:check_revision"
 end

a5525159611020ebade2544a295cbe1300d5a865
Marcos Serpa <marcosserpa@gmail.com>
2013-11-19 11:52:08 -0200

    Fixing contact email destination.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
 
-  default from: "guest@codepolaris.com"
+  default from: "contato@codepolaris.com"
 
   def contact_email(params)
     @guest = params
 
-    mail(to: 'marcosserpa@gmail.com', from: @guest[:email],
+    mail(to: 'contato@codepolaris.com', from: @guest[:email],
          subject: @guest[:subject].present? ? @guest[:subject] : 'Site Contact')
   end
 

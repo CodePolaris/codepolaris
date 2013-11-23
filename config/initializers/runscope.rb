eb6117b33711d83075fe17af29f5e8d1c214c897
Marcos Serpa <marcosserpa@gmail.com>
2013-11-23 21:52:04 -0200

    Runscope settings.

--- config/initializers/runscope.rb
+++ config/initializers/runscope.rb
@@ -1,4 +1,4 @@
 Runscope.configure do |config|
   config.bucket = "rzts72x1hz89"
-  config.domains = ["http://codepolaris-com-rzts72x1hz89.runscope.net/"]
+  config.domains = ["http://codepolaris-com-rzts72x1hz89.runscope.net/", "codepolaris.com"]
 end
